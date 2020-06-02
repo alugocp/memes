b74b7e6848e4929883a3a167950bebe1217f662d
alugocp <alugocp@aim.com>
2020-06-02 12:16:26 -0400

    Fixed SERVER variable for live site

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
-  const SERVER="http://localhost:2017";
+  //const SERVER="http://localhost:2017/";
+  const SERVER="";
   const CLUSTERS=14;
   let cluster=null;
   let clusterid=0;
 
   // Content population
   let loadVideo = (name,type) => $(".content").empty().append(
-    $(`<video type="${type}" controls><source src="${SERVER}/content/${name}"></video>`)
+    $(`<video type="${type}" controls><source src="${SERVER}content/${name}"></video>`)
   );
   let loadImage = (name,type) => $(".content").empty().append(
-    $(`<img src="${SERVER}/content/${name}"/>`)
+    $(`<img src="${SERVER}content/${name}"/>`)
   );
 
   // Cluster loading
   function loadCluster(cb){
     let i=Math.floor(Math.random()*CLUSTERS);
     if(i==clusterid) i=(CLUSTERS-i)-1;
-    $.get(`${SERVER}/clusters/cluster${i}.json`,null,function(res){
+    $.get(`${SERVER}clusters/cluster${i}.json`,null,function(res){
       cluster=(typeof(res)=="object")?res:JSON.parse(res);
       clusterid=i;
       if(cb) cb();
